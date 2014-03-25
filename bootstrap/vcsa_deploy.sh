c42ccc6f6241a441753fcb4028fb4329f561e39e
Tim Green <greent@vmware.com>
2014-03-25 18:52:26 -0400

    add ovf deploy helper script

---
+++ bootstrap/vcsa_deploy.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# VMware-ovftool-3.5.0-1274719-lin.x86_64.bundle should already be installed.
+# use VMware-ovftool-3.5.0-1274719-lin.x86_64.bundle --extract <dest_dir> for custom (unattended) install.
+
+help()
+{
+  echo 'this script deploys the VCSA and configures it using puppet'
+  echo ''
+  echo 'vcsa_deploy.sh <appliance_file>'
+  echo ''
+}
+
+# run the vcsa configuration manifest
+puppet_run()
+{
+    puppet apply --modulepath=$1/modules $1/manifests/vmware-vcsa/manifests/init.pp
+}
+
+if [ $# -ne 1 ]; then
+  help
+elif [ ! -f $PWD/vcsa.conf ]; then
+  echo '[ERROR]: unable to find required configuration file vcsa.conf'
+  exit 1
+fi
+
+OVF_APPLIANCE=$1
+
+source $PWD/vcsa.conf
+
+$OVFTOOL --acceptAllEulas --network="${MGMT_NETWORK}" --datastore="${DATASTORE}" --powerOn --vmFolder=${VM_FOLDER} --prop:vami.hostname=${VSCA_HOSTNAME} \
+  ${OVF_APPLIANCE} vi://${VCSA_USERNAME}:${VCSA_PASSWORD}@${TARGET_VCENTER}/${TARGET_HOST}
+
+# wait for ssh to come up on the newly provisioned appliance
+sleep 60
+
+PUPDIR=`dirname $PWD`
+puppet_run $PUPDIR
